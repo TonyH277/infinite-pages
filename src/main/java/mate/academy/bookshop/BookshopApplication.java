2c1069e48c8bf24c786e0421db2a36195d41c075
TonyH277 <robhobs50@gmail.com>
2024-04-18 18:15:11 +0300

    book init commit

--- src/main/java/mate/academy/bookshop/BookshopApplication.java
+++ src/main/java/mate/academy/bookshop/BookshopApplication.java
@@ -1,13 +1,35 @@
 package mate.academy.bookshop;
 
+import java.math.BigDecimal;
+import mate.academy.bookshop.model.Book;
+import mate.academy.bookshop.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BookshopApplication {
+    @Autowired
+    private BookService bookService;
 
-	public static void main(String[] args) {
-		SpringApplication.run(BookshopApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BookshopApplication.class, args);
+    }
 
+    @Bean
+    public CommandLineRunner commandLineRunner() {
+        return args -> {
+            Book book = new Book();
+            book.setTitle("Mavka");
+            book.setAuthor("Marichka");
+            book.setDescription("Description");
+            book.setIsbn("ISBN 123456");
+            book.setPrice(BigDecimal.TEN);
+
+            bookService.save(book);
+            bookService.findAll().forEach(System.out::println);
+        };
+    }
 }
