2cf77498d494414b13b6ea0f43c45a33e6d8ebc7
TonyH277 <robhobs50@gmail.com>
2024-04-25 14:52:09 +0300

    add specification

---
+++ src/main/java/mate/academy/bookshop/dto/BookSearchParametersDto.java
@@ -0,0 +1,4 @@
+package mate.academy.bookshop.dto;
+
+public class BookSearchParametersDto {
+}
--- src/main/java/mate/academy/bookshop/repository/BookRepository.java
+++ src/main/java/mate/academy/bookshop/repository/BookRepository.java
@@ -2,8 +2,9 @@ package mate.academy.bookshop.repository;
 
 import mate.academy.bookshop.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BookRepository extends JpaRepository<Book, Long> {
+public interface BookRepository extends JpaRepository<Book, Long>, JpaSpecificationExecutor<Book> {
 }
