ed42e5c904ffa084c13fcca10bd255feb7b21c83
Anton Haiduk <157686954+TonyH277@users.noreply.github.com>
2024-04-26 14:09:00 +0300

    add JPA Repository to project & add put, delete endpoints (#3)
    * add JPA Repository to project & add put, delete endpoints
    
    * add open-in-view=false
    
    * update hibernate-core to 6.4.5 version
    
    * remove redundant imports

--- src/main/java/mate/academy/bookshop/service/impl/BookServiceImpl.java
+++ src/main/java/mate/academy/bookshop/service/impl/BookServiceImpl.java
@@ -36,4 +36,24 @@ public class BookServiceImpl implements BookService {
         return bookMapper.toDto(bookRepository.findById(id).orElseThrow(()
                 -> new EntityNotFoundException("Can't find book with id " + id)));
     }
+
+    @Override
+    public BookDto update(Long id, CreateBookRequestDto requestDto) {
+        Book book = bookRepository.findById(id).orElseThrow(()
+                -> new EntityNotFoundException("Can't find book with id " + id));
+
+        book.setAuthor(requestDto.getAuthor());
+        book.setCoverImage(requestDto.getCoverImage());
+        book.setDescription(requestDto.getDescription());
+        book.setIsbn(requestDto.getIsbn());
+        book.setPrice(requestDto.getPrice());
+        book.setTitle(requestDto.getTitle());
+
+        return bookMapper.toDto(bookRepository.save(book));
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        bookRepository.deleteById(id);
+    }
 }
