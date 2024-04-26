eace50590a453caf5f980cacf32b9e1e6c24ce5b
TonyH277 <robhobs50@gmail.com>
2024-04-26 09:24:54 +0300

    implement criteria query for titles & authors

---
+++ src/main/java/mate/academy/bookshop/repository/bookSpecificaiton/BookSpecificationProviderManager.java
@@ -0,0 +1,22 @@
+package mate.academy.bookshop.repository.bookSpecificaiton;
+
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import mate.academy.bookshop.model.Book;
+import mate.academy.bookshop.repository.SpecificationProvider;
+import mate.academy.bookshop.repository.SpecificationProviderManager;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class BookSpecificationProviderManager implements SpecificationProviderManager<Book> {
+    private final List<SpecificationProvider<Book>> bookSpecificationProviders;
+
+    @Override
+    public SpecificationProvider<Book> getSpecificationProvider(String key) {
+        return bookSpecificationProviders.stream()
+                .filter(b -> b.getKey().equals(key))
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("Can't find correct specification provider for key " + key));
+    }
+}
