e313fb23a06ea927477618af4977696f649aedc6
TonyH277 <robhobs50@gmail.com>
2024-04-28 21:55:47 +0300

    improve code at BookSpecificationBuilder

--- src/main/java/mate/academy/bookshop/repository/book/BookSpecificationBuilder.java
+++ src/main/java/mate/academy/bookshop/repository/book/BookSpecificationBuilder.java
@@ -11,34 +11,28 @@ import org.springframework.stereotype.Component;
 @Component
 @RequiredArgsConstructor
 public class BookSpecificationBuilder implements SpecificationBuilder<Book> {
+    private static final String AUTHOR = "author";
+    private static final String TITLE = "title";
+    private static final String PRICE = "price";
+    private static final String ISBN = "isbn";
     private final SpecificationProviderManager<Book> bookSpecificationProviderManager;
 
     @Override
     public Specification<Book> build(BookSearchParametersDto searchParameters) {
         Specification<Book> specification = Specification.where(null);
-        if (searchParameters.getAuthors() != null
-                && searchParameters.getAuthors().length > 0) {
+        specification = addSpecification(specification, searchParameters.getAuthors(), AUTHOR);
+        specification = addSpecification(specification, searchParameters.getTitles(), TITLE);
+        specification = addSpecification(specification, searchParameters.getPriceRange(), PRICE);
+        specification = addSpecification(specification, searchParameters.getIsbns(), ISBN);
+        return specification;
+    }
+
+    private Specification<Book> addSpecification(Specification<Book> specification,
+                                                 String[] values, String field) {
+        if (values != null && values.length > 0) {
             specification = specification.and(bookSpecificationProviderManager
-                    .getSpecificationProvider("author")
-                    .getSpecification(searchParameters.getAuthors()));
-        }
-        if (searchParameters.getTitles() != null
-                && searchParameters.getTitles().length > 0) {
-            specification = specification.and(bookSpecificationProviderManager
-                    .getSpecificationProvider("title")
-                    .getSpecification(searchParameters.getTitles()));
-        }
-        if (searchParameters.getPriceRange() != null
-                && searchParameters.getPriceRange().length > 0) {
-            specification = specification.and(bookSpecificationProviderManager
-                    .getSpecificationProvider("price")
-                    .getSpecification(searchParameters.getPriceRange()));
-        }
-        if (searchParameters.getIsbns() != null
-                && searchParameters.getIsbns().length > 0) {
-            specification = specification.and(bookSpecificationProviderManager
-                    .getSpecificationProvider("isbn")
-                    .getSpecification(searchParameters.getIsbns()));
+                    .getSpecificationProvider(field)
+                    .getSpecification(values));
         }
         return specification;
     }
