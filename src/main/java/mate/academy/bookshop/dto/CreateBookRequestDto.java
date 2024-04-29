7299b7369eac9f29253a4896af930adbd00f08f1
TonyH277 <robhobs50@gmail.com>
2024-04-29 21:42:23 +0300

    implement CustomGlobalExceptionHandler

--- src/main/java/mate/academy/bookshop/dto/CreateBookRequestDto.java
+++ src/main/java/mate/academy/bookshop/dto/CreateBookRequestDto.java
@@ -1,15 +1,24 @@
 package mate.academy.bookshop.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import lombok.Data;
 
 @Data
 public class CreateBookRequestDto {
+    @NotNull
     private String title;
+    @NotNull
     private String author;
+    @NotNull
     private String isbn;
+    @NotNull
+    @Min(0)
     private BigDecimal price;
+    @NotNull
     private String description;
+    @NotNull
     private String coverImage;
 }
 
