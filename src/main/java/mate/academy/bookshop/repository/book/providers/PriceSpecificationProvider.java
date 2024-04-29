787580ddda6a2019224f28652a80726ee366aebf
TonyH277 <robhobs50@gmail.com>
2024-04-29 16:03:06 +0300

    update PriceSpecificationProvider

--- src/main/java/mate/academy/bookshop/repository/book/providers/PriceSpecificationProvider.java
+++ src/main/java/mate/academy/bookshop/repository/book/providers/PriceSpecificationProvider.java
@@ -8,7 +8,9 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class PriceSpecificationProvider implements SpecificationProvider<Book> {
-
+    private static final int EXPECTED_LENGTH = 2;
+    private static final int MIN_PRICE_INDEX = 0;
+    private static final int MAX_PRICE_INDEX = 1;
     @Override
     public String getKey() {
         return "price";
@@ -16,11 +18,11 @@ public class PriceSpecificationProvider implements SpecificationProvider<Book> {
 
     @Override
     public Specification<Book> getSpecification(String[] params) {
-        if (params == null || params.length != 2) {
+        if (params == null || params.length != EXPECTED_LENGTH) {
             throw new IllegalArgumentException("Invalid price range parameters");
         }
-        BigDecimal minPrice = BigDecimal.valueOf(Long.parseLong(params[0]));
-        BigDecimal maxPrice = BigDecimal.valueOf(Long.parseLong(params[1]));
+        BigDecimal minPrice = BigDecimal.valueOf(Long.parseLong(params[MIN_PRICE_INDEX]));
+        BigDecimal maxPrice = BigDecimal.valueOf(Long.parseLong(params[MAX_PRICE_INDEX]));
 
         return (root, query, criteriaBuilder)
                 -> criteriaBuilder.between(root.get("price"), minPrice, maxPrice);
