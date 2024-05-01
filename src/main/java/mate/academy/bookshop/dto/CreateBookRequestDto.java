f33972ae2ef869fafb73e6fe78d7714b5d31ed48
TonyH277 <robhobs50@gmail.com>
2024-05-01 11:02:09 +0300

    add meaningful validation for CreateBookRequestDto

--- src/main/java/mate/academy/bookshop/dto/CreateBookRequestDto.java
+++ src/main/java/mate/academy/bookshop/dto/CreateBookRequestDto.java
@@ -1,24 +1,34 @@
 package mate.academy.bookshop.dto;
 
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import java.math.BigDecimal;
 import lombok.Data;
 
 @Data
 public class CreateBookRequestDto {
-    @NotNull
+    @NotBlank(message = "title can not be blank")
+    @Size(max = 255, message = "max title length is 255 characters")
     private String title;
-    @NotNull
+
+    @NotBlank(message = "message can not be blank")
+    @Size(max = 255, message = "max author length is 255 characters")
     private String author;
-    @NotNull
+
+    @NotBlank(message = "isbn can not be blank")
+    @Size(min = 10, message = "min isbn length is 10 characters")
+    @Size(max = 255, message = "max isbn length is 255 characters")
     private String isbn;
-    @NotNull
-    @Min(0)
+
+    @NotNull(message = "price can not be null")
+    @Min(value = 0, message = "min price value is 0")
     private BigDecimal price;
-    @NotNull
+
+    @NotBlank(message = "description can not be blank")
     private String description;
-    @NotNull
+
     private String coverImage;
 }
 
