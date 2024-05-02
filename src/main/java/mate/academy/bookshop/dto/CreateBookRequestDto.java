7058ec0ab1758ca2d2f759e85b30ecdc4ff48bd3
Anton Haiduk <157686954+TonyH277@users.noreply.github.com>
2024-05-02 15:04:07 +0300

    Global exception handler (#5)
    * implement CustomGlobalExceptionHandler
    
    * fix imports
    
    * move Strings to constants
    
    * add meaningful validation for CreateBookRequestDto
    
    * add EntityNotFoundException to CustomGlobalExceptionHandler
    
    * add InvalidPriceRangeException to CustomGlobalExceptionHandler
    
    * update pom.xml

--- src/main/java/mate/academy/bookshop/dto/CreateBookRequestDto.java
+++ src/main/java/mate/academy/bookshop/dto/CreateBookRequestDto.java
@@ -1,15 +1,34 @@
 package mate.academy.bookshop.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import java.math.BigDecimal;
 import lombok.Data;
 
 @Data
 public class CreateBookRequestDto {
+    @NotBlank(message = "can not be blank")
+    @Size(max = 255, message = "max length is 255 characters")
     private String title;
+
+    @NotBlank(message = "can not be blank")
+    @Size(max = 255, message = "max length is 255 characters")
     private String author;
+
+    @NotBlank(message = "can not be blank")
+    @Size(min = 10, message = "min length is 10 characters")
+    @Size(max = 255, message = "max length is 255 characters")
     private String isbn;
+
+    @NotNull(message = "can not be null")
+    @Min(value = 0, message = "min value is 0")
     private BigDecimal price;
+
+    @NotBlank(message = "can not be blank")
     private String description;
+
     private String coverImage;
 }
 
--- src/main/java/mate/academy/bookshop/repository/book/providers/PriceSpecificationProvider.java
+++ src/main/java/mate/academy/bookshop/repository/book/providers/PriceSpecificationProvider.java
@@ -1,6 +1,7 @@
 package mate.academy.bookshop.repository.book.providers;
 
 import java.math.BigDecimal;
+import mate.academy.bookshop.exception.InvalidPriceRangeException;
 import mate.academy.bookshop.model.Book;
 import mate.academy.bookshop.repository.SpecificationProvider;
 import org.springframework.data.jpa.domain.Specification;
@@ -8,21 +9,29 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class PriceSpecificationProvider implements SpecificationProvider<Book> {
+    private static final String PRICE = "price";
+    private static final int EXPECTED_LENGTH = 2;
+    private static final int MIN_PRICE_INDEX = 0;
+    private static final int MAX_PRICE_INDEX = 1;
 
     @Override
     public String getKey() {
-        return "price";
+        return PRICE;
     }
 
     @Override
     public Specification<Book> getSpecification(String[] params) {
-        if (params == null || params.length != 2) {
-            throw new IllegalArgumentException("Invalid price range parameters");
-        }
-        BigDecimal minPrice = BigDecimal.valueOf(Long.parseLong(params[0]));
-        BigDecimal maxPrice = BigDecimal.valueOf(Long.parseLong(params[1]));
+        try {
+            if (params == null || params.length != EXPECTED_LENGTH) {
+                throw new InvalidPriceRangeException("Price range should contain strict 2 params");
+            }
+            BigDecimal minPrice = BigDecimal.valueOf(Long.parseLong(params[MIN_PRICE_INDEX]));
+            BigDecimal maxPrice = BigDecimal.valueOf(Long.parseLong(params[MAX_PRICE_INDEX]));
 
-        return (root, query, criteriaBuilder)
-                -> criteriaBuilder.between(root.get("price"), minPrice, maxPrice);
+            return (root, query, criteriaBuilder)
+                    -> criteriaBuilder.between(root.get(PRICE), minPrice, maxPrice);
+        } catch (NumberFormatException e) {
+            throw new InvalidPriceRangeException("Price range should contain only numbers");
+        }
     }
 }
