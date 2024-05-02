e763110ee0c57548e9bbcd991324de4769bf5477
TonyH277 <robhobs50@gmail.com>
2024-05-02 15:20:30 +0300

    update BookRepository

--- src/main/java/mate/academy/bookshop/repository/book/BookRepository.java
+++ src/main/java/mate/academy/bookshop/repository/book/BookRepository.java
@@ -1,17 +1,10 @@
 package mate.academy.bookshop.repository.book;
 
-import java.util.List;
 import mate.academy.bookshop.model.Book;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long>, JpaSpecificationExecutor<Book> {
-    Page<Book> findAll(Pageable pageable);
-
-    List<Book> findAll(Sort sort);
 }
