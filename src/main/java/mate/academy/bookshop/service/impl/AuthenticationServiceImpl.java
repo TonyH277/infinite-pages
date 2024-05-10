ce56d9be17d532b80879e4210fc3aaa78a692aab
TonyH277 <robhobs50@gmail.com>
2024-05-10 09:28:02 +0300

    add roles during registration flow

--- src/main/java/mate/academy/bookshop/service/impl/AuthenticationServiceImpl.java
+++ src/main/java/mate/academy/bookshop/service/impl/AuthenticationServiceImpl.java
@@ -1,5 +1,7 @@
 package mate.academy.bookshop.service.impl;
 
+import java.util.Set;
+import javax.management.relation.Role;
 import lombok.RequiredArgsConstructor;
 import mate.academy.bookshop.dto.UserRegistrationRequestDto;
 import mate.academy.bookshop.dto.UserResponseDto;
