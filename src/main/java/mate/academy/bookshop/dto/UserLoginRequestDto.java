7e5067389d065bb9d23b174232fdc5201af88575
TonyH277 <robhobs50@gmail.com>
2024-05-15 11:08:24 +0300

    add swagger to AuthenticationController

--- src/main/java/mate/academy/bookshop/dto/UserLoginRequestDto.java
+++ src/main/java/mate/academy/bookshop/dto/UserLoginRequestDto.java
@@ -2,12 +2,15 @@ package mate.academy.bookshop.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public record UserLoginRequestDto(
-        @Email
-        @NotBlank(message = "can not be blank")
+        @Email(message = "Invalid email format")
+        @NotBlank(message = "Email can not be blank")
+        @Size(max = 255, message = "Email must not exceed {max} characters")
         String email,
 
-        @NotBlank(message = "can not be blank")
+        @NotBlank(message = "Password can not be blank")
+        @Size(min = 4, max = 255, message = "Password must be between {min} and {max} characters")
         String password) {
 }
