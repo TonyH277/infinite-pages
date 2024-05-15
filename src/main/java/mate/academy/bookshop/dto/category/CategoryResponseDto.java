29cf30328d63b8c7a224155992bcb1f5fbc10465
TonyH277 <robhobs50@gmail.com>
2024-05-15 19:04:39 +0300

    update codestyle

--- src/main/java/mate/academy/bookshop/dto/category/CategoryResponseDto.java
+++ src/main/java/mate/academy/bookshop/dto/category/CategoryResponseDto.java
@@ -3,5 +3,6 @@ package mate.academy.bookshop.dto.category;
 public record CategoryResponseDto(
         Long id,
         String name,
-        String description) {
+        String description
+) {
 }
