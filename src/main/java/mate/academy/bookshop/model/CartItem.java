921af3ca515f5cd75bd9d8b41f2305e81faf395e
Anton Haiduk <157686954+TonyH277@users.noreply.github.com>
2024-05-22 11:45:10 +0300

    update FetchType at CartItem & ShoppingCart (#14)

--- src/main/java/mate/academy/bookshop/model/CartItem.java
+++ src/main/java/mate/academy/bookshop/model/CartItem.java
@@ -1,12 +1,12 @@
 package mate.academy.bookshop.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -23,10 +23,10 @@ public class CartItem {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "shopping_cart_id", nullable = false)
     private ShoppingCart shoppingCart;
-    @OneToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "book_id", nullable = false)
     private Book book;
     private int quantity;
